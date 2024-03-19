8a08758c9f568637d452e64a37ae82f51a1c77ab
mmuukkeesshh <126387293+mmuukkeesshh@users.noreply.github.com>
2024-03-19 09:31:42 +0530

    counter

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Counter from './Components/Counter';
+import './App.css'
+
+
+export default function App(){
+    return (
+        <div id="counter">
+            <Counter/>
+        </div>
+    );
+}
---
+++ src/Components/Counter.jsx
@@ -0,0 +1,26 @@
+
+import { useState } from 'react';
+import './Counter.css';
+
+export default function Counter(){
+    var [Value,setCounterValue]=useState(0);
+    function plus(){
+        setCounterValue(++Value);
+
+    }
+    function minus(){
+      setCounterValue(--Value);
+
+    }
+    return (
+        <div id="body">
+           <div className="Counter_container">Counter</div>
+           <div id="jhansi">
+           <div className="minus" onClick={()=>minus()}>-</div>   
+           <div id="number">{Value}</div>
+           <div className="plus" onClick={()=>plus()}>+</div>
+           </div>
+        </div>
+       
+    );
+}
